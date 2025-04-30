e2fa4861e4f1f3b25925b59d91c81f69ceb74d90
mmyersbyte <pedrovictorcastrodesousa@gmail.com>
2025-04-30 20:01:10 -0300

    ajuste no cors, autenticação por cookie

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,12 @@ const PORT = process.env.PORT || 5555;
 dotenv.config();
 connectDB(process.env.MONGODB_URI);
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://www.labemunisul.com.br',
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/api', contactRoutes);
