d1f6393bcce78f2caea595df541e235a6d61ee2b
mmyersbyte <pedrovictorcastrodesousa@gmail.com>
2025-06-26 14:56:01 -0300

    route health

--- backend/src/app.js
+++ backend/src/app.js
@@ -38,6 +38,7 @@ app.use(
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true, limit: '2mb' }));
 app.use(cookieParser());
+app.use('/health', (req, res) => res.send('ok')); // Rota health pública
 app.use('/', rootRoutes);
 app.use('/auth', authRoutes);
 app.use('/api/contact', contactRoutes);
